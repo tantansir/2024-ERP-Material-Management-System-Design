a235c35c9353cc95e8bbbba10ec289d22473cce0
2151825 <2151825@tongji.edu.cn>
2024-08-04 17:06:23 +0800

    更新了收货和发票部分内容

--- MM/views/ajax/receipt_api.py
+++ MM/views/ajax/receipt_api.py
@@ -18,31 +18,48 @@ from ..auxiliary import *
 
 @login_required
 def search_receipt(request: HttpRequest):
+    """
+    根据用户ID或商品收据ID搜索商品收据信息。
+
+    - 如果请求方法不是POST，返回405状态码的HTTP响应。
+    - 如果请求方法是POST：
+      - 从请求的POST数据中获取'pk'，如果'pk'为空或None，则从'uid'获取用户ID，并根据是否有日期信息进行查询。
+      - 如果提供了日期信息，将根据用户ID和日期进行查询。
+      - 如果没有提供日期信息，只根据用户ID进行查询。
+      - 查询到的商品收据信息将被序列化为JSON格式，并返回包含状态、消息和商品收据数据的HTTP响应。
+      - 如果提供了商品收据的ID（pk），将根据此ID进行精确查询。
+        - 如果查询结果不是恰好一个，返回状态为0和错误消息的HTTP响应。
+        - 如果查询结果恰好一个，返回状态为1，消息为成功，以及商品收据数据的HTTP响应。
+
+    参数:
+    - request: HttpRequest对象，包含请求方法和请求数据。
+
+    返回:
+    - 如果请求方法不是POST，返回405状态码的HTTP响应。
+    - 如果请求方法是POST，根据查询结果返回JSON格式的HTTP响应。
+    """
+    # 如果请求方法不是POST，返回405状态码的HTTP响应
     if request.method != 'POST':
         return HttpResponse(status=405)
+    # 从请求的POST数据中获取数据
     post = request.POST
     pk = post.get('pk')
+    # 如果pk为空或None，从uid获取用户ID，并进行查询
     if pk == '' or pk is None:
         uid = getPk(post.get('uid'), 'U')
         date = post.get('date')
+        # 根据是否有日期信息，设置查询条件
         if date == '':
             results: QuerySet = GoodReceipt.objects.filter(
                 euser__uid__regex=uid
             )
         else:
             dateInfo = date.split('. ')
-            # startDate = datetime.datetime(
-            #     year=int(dateInfo[2]), month=int(dateInfo[0]), day=int(dateInfo[1]),
-            #     hour=0,minute=0,second=0, tzinfo=pytz.UTC
-            # )
-            # endDate = datetime.datetime(
-            #     year=int(dateInfo[2]), month=int(dateInfo[0]), day=int(dateInfo[1]),
-            #     hour=23,minute=59,second=59, tzinfo=pytz.UTC
-            # )
             results: QuerySet = GoodReceipt.objects.filter(
-                euser__uid__regex=uid, time__year=int(dateInfo[2]), 
+                euser__uid__regex=uid, time__year=int(dateInfo[2]),
                 time__month=int(dateInfo[0]), time__day=int(dateInfo[1])
             )
+        # 序列化查询结果
         results_list = json.loads(serializers.serialize('json', list(results)))
         for i, r in enumerate(results):
             user: EUser = EUser.objects.get(pk__exact=r.euser.pk)
@@ -58,6 +75,7 @@ def search_receipt(request: HttpRequest):
             results_list[i]['material'] = model_to_dict(material)
         return HttpResponse(json.dumps({'status':1, 'message':"商品收据检索成功！", 'gr':results_list}, default=str))
     else:
+        # 如果提供了商品收据的ID，进行精确查询
         pk = getPkExact(pk, 'GR')
         results: QuerySet = GoodReceipt.objects.filter(pk__exact=pk)
         if len(results) != 1:
@@ -79,8 +97,22 @@ def search_receipt(request: HttpRequest):
                 results_list[i]['material'] = model_to_dict(material)
             return HttpResponse(json.dumps({'status':1, 'message':"商品收据检索成功！", 'gr':results_list}, default=str))
 
+
 @login_required
 def create_receipt(request: HttpRequest):
+    """
+    创建收据
+
+    根据用户提交的表单数据，创建一个新的收据记录。这包括验证表单数据的准确性，
+    保存收据信息，更新订单状态和相关分数，以及更新库存和会计记录。
+
+    参数:
+    - request: HttpRequest对象，包含用户提交的表单数据。
+
+    返回:
+    - HttpResponse对象，包含操作状态和相关消息的JSON数据。
+    """
+    # 获取表单数据
     post = request.POST
     po_id = getPkExact(post.get('po_id'), 'PO')
     oi_itemId = getPkExact(post.get('oi_id'), 'OI')
@@ -90,22 +122,31 @@ def create_receipt(request: HttpRequest):
     serviceScore = post.get('serviceScore')
     postTime = getDate(post.get('postTime'))
     time = getDate(post.get('time'))
-    
+
+    # 获取订单项和用户信息
     orderItem: OrderItem = OrderItem.objects.get(po__id__exact=po_id, itemId=oi_itemId)
     euser = EUser.objects.get(pk__exact=request.user.id)
+
+    # 创建新的收据对象
     new_gr = GoodReceipt(
         actualQnty=actualQnty, sType=sType, time=time, postTime=postTime, orderItem=orderItem, euser=euser
     )
+
+    # 验证收据数据
     try:
         new_gr.full_clean()
     except ValidationError as e:
         error_fields = list(e.error_dict.keys())
         return HttpResponse(json.dumps({'status':0, 'message':"表单填写错误！", 'fields':error_fields}))
+
+    # 保存收据并更新订单状态
     new_gr.save()
     orderItem.status = '1'
     orderItem.qualityScore = int(qualityScore) * 20
     orderItem.serviceScore = int(serviceScore) * 20
     orderItem.quantityScore = actualQnty / orderItem.quantity * 100
+
+    # 计算及时性分数
     dif = (time - orderItem.deliveryDate).days
     score = 0
     if dif <= 0: score=100
@@ -114,32 +155,51 @@ def create_receipt(request: HttpRequest):
     elif 15 <= dif < 30: score=40
     else: score= 20
     orderItem.ontimeScore = score
+
+    # 保存订单更新
     orderItem.save()
+
+    # 获取物料项信息
     materialItem: MaterialItem = MaterialItem.objects.get(pk__exact=orderItem.meterialItem.id)
 
+    # 创建新的库存历史记录
     new_stockHistory = StockHistory(
         item=materialItem, type='1', unrestrictUse=materialItem.unrestrictUse,
         blocked=materialItem.blocked, qltyInspection=materialItem.qltyInspection,
-        time=time, 
+        time=time,
     )
+
+    # 验证库存历史数据
     try:
         new_stockHistory.full_clean()
     except ValidationError as e:
         error_fields = list(e.error_dict.keys())
         return HttpResponse(json.dumps({'status':0, 'message':"表单填写错误！", 'fields':error_fields}))
+
+    # 根据收据类型更新库存数量
     if sType=='1': materialItem.blocked += actualQnty
     elif sType=='2': materialItem.qltyInspection += actualQnty
     elif sType=='3': materialItem.unrestrictUse += actualQnty
+
+    # 保存库存更新
     materialItem.save()
+
+    # 创建新的会计记录
     new_account = Account(
         sumAmount=orderItem.price * actualQnty, JEType='WE', postDate=postTime
     )
+
+    # 验证会计记录数据
     try:
         new_account.full_clean()
     except ValidationError as e:
         error_fields = list(e.error_dict.keys())
         return HttpResponse(json.dumps({'status':0, 'message':"表单填写错误！", 'fields':error_fields}))
+
+    # 保存会计记录
     new_account.save()
+
+    # 创建会计明细记录
     accountDetail1 = AccountDetail(
         glAccount='200200', type='0', amount=orderItem.price * actualQnty, je=new_account
     )
@@ -147,12 +207,19 @@ def create_receipt(request: HttpRequest):
         glAccount='310000', type='1', amount=orderItem.price * actualQnty,
         gr=new_gr, je=new_account
     )
+
+    # 验证会计明细数据
     try:
         accountDetail1.full_clean()
         accountDetail2.full_clean()
     except ValidationError as e:
         error_fields = list(e.error_dict.keys())
         return HttpResponse(json.dumps({'status':0, 'message':"表单填写错误！", 'fields':error_fields}))
+
+    # 保存会计明细记录
     accountDetail1.save()
     accountDetail2.save()
+
+    # 返回成功消息
     return HttpResponse(json.dumps({'status':1, 'message':"商品收据创建成功！收据编号为"+str(new_gr.id)+"。"}))
+
--- MM/views/receipt.py
+++ MM/views/receipt.py
@@ -14,41 +14,99 @@ from .auxiliary import *
 
 @login_required
 def create_receipt(request: HttpRequest):
+    """
+    根据请求创建收据。
+
+    该函数用于处理创建收据的HTTP请求。如果请求方法为GET，则会渲染并返回创建收据的页面；
+    如果请求方法不是GET，则返回一个405错误，表示方法不允许。
+
+    参数:
+    - request: HttpRequest对象，包含请求的元数据和方法。
+
+    返回值:
+    - 如果请求方法为GET，返回渲染后的创建收据页面。
+    - 如果请求方法不是GET，返回一个状态码为405的HttpResponse对象。
+    """
+    # 对于GET请求，调用render函数渲染并返回指定的模板页面
     if request.method == 'GET':
         return render(
             request=request,
             template_name='../templates/receipt/create.html'
         )
+    # 对于非GET请求，返回一个表示方法不允许的HTTP响应
     else:
         return HttpResponse(status=405)
 
 @login_required
 def search_receipt(request: HttpRequest):
+    """
+    根据HTTP请求方法处理收据搜索逻辑。
+
+    如果请求方法为GET，则提供搜索收据的表单界面。
+    对于任何非GET方法的请求，返回405状态码，表示方法不允许。
+
+    参数:
+    - request: HttpRequest对象，包含来自客户端的请求信息。
+
+    返回值:
+    - 对于GET请求，返回一个渲染后的搜索收据的HTML页面。
+    - 对于非GET请求，返回一个HttpResponse对象，状态码为405。
+    """
     if request.method == 'GET':
+        # GET请求，返回搜索收据的表单界面
         return render(
             request=request,
             template_name='../templates/receipt/search.html'
         )
     else:
+        # 非GET请求，返回405状态码，表示方法不允许
         return HttpResponse(status=405)
 
 @login_required
 def display_receipt(request: HttpRequest):
+    """
+    根据请求显示收据信息。
+
+    该函数从HTTP请求中获取订单ID和物品ID，然后查询和组装相关数据，
+    最后将数据传递给模板进行渲染，生成收据页面。
+
+    :param request: HttpRequest对象，包含前端发送的请求信息
+    :return: 渲染后的收据页面
+    """
+    # 从请求中获取查询参数
     get = request.GET
     orderID = get.get('orderID')
     itemID = get.get('itemID')
+
+    # 根据订单ID和物品ID查询OrderItem对象，如果不存在则返回404错误
     item: OrderItem = get_object_or_404(OrderItem, po__id__exact=orderID, itemId__exact=itemID)
+
+    # 查询对应的GoodReceipt对象
     gr = GoodReceipt.objects.get(orderItem__id__exact=item.id)
+
+    # 将OrderItem对象转换为字典
     item_dict = model_to_dict(item)
+
+    # 查询MaterialItem对象，并将其添加到字典中
     materialItem: MaterialItem = get_object_or_404(MaterialItem, id__exact=item.meterialItem.id)
+    item_dict['materialItem'] = model_to_dict(materialItem)
+
+    # 查询Material对象，并将其添加到字典中
     material: Material = get_object_or_404(Material, id__exact=materialItem.material.id)
+    item_dict['material'] = model_to_dict(material)
+
+    # 查询Stock对象，并将其添加到字典中
     stock: Stock = get_object_or_404(Stock, id__exact=materialItem.stock.id)
+    item_dict['stock'] = model_to_dict(stock)
+
+    # 查询PurchaseOrder对象，并将其添加到字典中
     po: PurchaseOrder = get_object_or_404(PurchaseOrder, id__exact=item.po.id)
     item_dict['po'] = model_to_dict(po)
-    item_dict['materialItem'] = model_to_dict(materialItem)
-    item_dict['material'] = model_to_dict(material)
-    item_dict['stock'] = model_to_dict(stock)
+
+    # 将GoodReceipt对象转换为字典，并添加到item_dict中
     item_dict['gr'] = model_to_dict(gr)
+
+    # 渲染模板并返回收据页面
     return render(
         request=request,
         template_name='../templates/receipt/display.html',
@@ -57,19 +115,49 @@ def display_receipt(request: HttpRequest):
 
 @login_required
 def load_order_item(request: HttpRequest):
+    """
+    根据请求加载订单项信息及其相关联的数据。
+
+    该函数从HttpRequest对象中获取订单ID和物品ID，然后根据这些ID获取订单项、物料项、物料和库存信息，
+    以及采购订单信息。这些信息被转换为字典形式，并传递给模板以供渲染。
+
+    参数:
+    - request: HttpRequest对象，包含订单ID和物品ID。
+
+    返回:
+    - 渲染后的模板，包含订单项及其相关数据。
+
+    解释:
+    1. 获取请求中的参数，包括订单ID和物品ID。
+    2. 使用这些ID获取订单项(OrderItem)、物料项(MaterialItem)、物料(Material)和库存(Stock)对象，
+       以及采购订单(PurchaseOrder)对象。
+    3. 将这些对象转换为字典形式，以便在模板中使用。
+    4. 渲染并返回包含这些数据的模板。
+    """
+    # 从GET请求中获取参数
     get = request.GET
     orderID = get.get('orderID')
     itemID = get.get('itemID')
+    
+    # 获取订单项信息
     item: OrderItem = get_object_or_404(OrderItem, po__id__exact=orderID, itemId__exact=itemID)
     item_dict = model_to_dict(item)
+    
+    # 获取物料项信息
     materialItem: MaterialItem = get_object_or_404(MaterialItem, id__exact=item.meterialItem.id)
     material: Material = get_object_or_404(Material, id__exact=materialItem.material.id)
     stock: Stock = get_object_or_404(Stock, id__exact=materialItem.stock.id)
+    
+    # 获取采购订单信息
     po: PurchaseOrder = get_object_or_404(PurchaseOrder, id__exact=item.po.id)
     item_dict['po_'] = model_to_dict(po)
+    
+    # 将相关联的对象添加到字典中
     item_dict['materialItem_'] = model_to_dict(materialItem)
     item_dict['material_'] = model_to_dict(material)
     item_dict['stock_'] = model_to_dict(stock)
+    
+    # 渲染并返回模板
     return render(
         request=request,
         template_name='../templates/receipt/create2.html',
@@ -78,29 +166,50 @@ def load_order_item(request: HttpRequest):
 
 @login_required
 def display_purchase_order(request: HttpRequest, pk):
+    """
+    显示采购订单详情页面。
+
+    根据提供的请求和采购订单ID（pk），检索相应的采购订单、供应商和订单项信息，
+    并计算订单总额。最终渲染并返回采购订单详情页面。
+
+    参数:
+    - request: HttpRequest对象，包含当前请求信息。
+    - pk: 采购订单的主键ID，用于查询具体订单。
+
+    返回:
+    - 渲染后的采购订单详情页面，包含订单、供应商和订单项等信息。
+    """
     if request.method == "GET":
+        # 获取指定ID的采购订单，"O"表示仅获取确认的订单
         pk = getPkExact(pk, "O")
+        # 根据ID获取采购订单详细信息
         purchaseOrder: PurchaseOrder = PurchaseOrder.objects.get(id=pk)
-        vendor: Vendor =  Vendor.objects.get(vid=purchaseOrder.vendor.vid)
-        orderItems  = OrderItem.objects.filter(po_id=pk).values(
+        # 根据采购订单获取对应的供应商信息
+        vendor: Vendor = Vendor.objects.get(vid=purchaseOrder.vendor.vid)
+        # 获取该采购订单的所有订单项，并选择需要的字段
+        orderItems = OrderItem.objects.filter(po_id=pk).values(
             "itemId","meterialItem__id","meterialItem__material__mname",
             "quantity","price","meterialItem__stock__id", "meterialItem__stock__name",
             "meterialItem__sloc","deliveryDate","po__rfq__rej","currency","status"
         )
+        # 将查询结果转换为列表，以便于后续处理
         orderItems = list(orderItems)
+        # 根据状态码将订单项的状态转换为易读的文本描述
         for i in orderItems:
             if i['status']=='0':
                 i['status']="货物未发出"
-            if i['status']=='1':
+            elif i['status']=='1':
                 i['status']="货物已送达"
-            if i['status']=='2':
-                i['status']="已收到发票"
-            if i['status']=='3':
+            elif i['status']=='2':
+                i['status']="已收到发货"
+            elif i['status']=='3':
                 i['status']="已完成支付"
+        # 计算每个订单项的小计，并累加到总金额
         sum = 0
         for i in orderItems:
             i['sum'] = i['quantity'] * i['price']
             sum += i['sum']
+        # 渲染并返回采购订单详情页面
         return render(
             request, '../templates/receipt/order.html',
             context={
@@ -112,35 +221,57 @@ def display_purchase_order(request: HttpRequest, pk):
 
 @login_required
 def search_orders(request):
+    """
+    根据请求方法处理订单搜索逻辑。
+
+    - 如果是GET请求，返回一个空的采购订单列表。
+    - 如果是POST请求，根据提交的参数过滤并返回相关的订单项。
+
+    参数:
+    - request: HttpRequest对象，包含请求方法（GET或POST）和请求数据。
+
+    返回:
+    - 对于GET请求，返回一个空的采购订单列表。
+    - 对于POST请求，返回根据过滤条件得到的订单项列表。
+    """
     if request.method == "GET":
+        # 初始化一个空的采购订单列表
         purchaseOrders = []
+        # 渲染并返回订单页面，此处不包含任何订单数据
         return render(request, '../templates/receipt/orders.html', context={"purchaseOrders":purchaseOrders})
     if request.method == "POST":
+        # 从POST请求中获取参数，并将它们转换为对应的主键
         id = request.POST.get("id"); id = getPk(id)
         ven = request.POST.get("ven"); ven = getPk(ven)
         mate = request.POST.get("mate"); mate = getPk(mate)
-        eu = request.POST.get("euser"); eu = getPk(eu)
+        eu = request.POST.get("eu"); eu = getPk(eu)
+        # 获取查询范围，并将其转换为对应的月数
         range = int(request.POST.get("range"))
         range_list = [12, 6, 3]
         now = timezone.now()
+        # 根据选择的时间范围计算查询的开始时间
         start = now + datetime.timedelta(days=-30 * range_list[range])
+        # 根据过滤条件查询订单项
         orderItems: QuerySet = OrderItem.objects.filter(
             po__id__regex=id, po__vendor__vid__regex=ven, meterialItem__material__id__regex=mate,
             po__euser__uid__regex=eu, po__time__range=[start, now]
         )
+        # 对查询结果进行聚合计算和排序
         orderItems: QuerySet = orderItems.values(
             "po__id", "itemId", "meterialItem__material__mname", "quantity", "price", "currency",
             "po__euser__uid", "po__vendor__vid", "po__time", "status", sum=F("quantity")*F("price")
         ).order_by("po__id")
+        # 将查询结果转换为列表
         orderItems = list(orderItems)
+        # 根据订单状态代码，将状态数字转换为对应的描述
         for i in orderItems:
             if i['status']=='0':
                 i['status']="货物未发出"
             if i['status']=='1':
                 i['status']="货物已送达"
             if i['status']=='2':
-                i['status']="已收到发票"
+                i['status']="已收到发货"
             if i['status']=='3':
                 i['status']="已完成支付"
-                i['rfq__ri__status'] = "已创建采购订单"
+        # 渲染并返回包含过滤后订单数据的页面
         return render(request, '../templates/receipt/orders.html', {"orderItems":orderItems})
